81954f8f15328508b782790f8bed7ab7da89c03b
blake-gifford <blakegifford01@gmail.com>
2021-07-10 20:36:40 -0700

    add updated client side with partial routing

--- surfreportapp/client/src/App.js
+++ surfreportapp/client/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import Articles from './components/Articles';
+import {Router} from '@reach/router';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Router>
+        <Dashboard path="/" />
+        <Home path="/home" />
+        <Articles path="/articles" />
+      </Router>
     </div>
   );
 }
---
+++ surfreportapp/client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const Home = () => {
+    // const 
+
+    return (
+        <div>
+            <h1>Welcome Home</h1>
+        </div>
+    )
+}
+
+export default Home
